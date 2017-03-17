d2a0d2fc5485e055b33dd5593172592666e698a2
Adrian Boyko <adrianboyko@gmail.com>
2017-03-17 12:18:32 -0700

    Small images were provided so added scaling.

--- diorama.py
+++ diorama.py
@@ -1,13 +1,12 @@
 # diorama.py controls the synchronized LED effects and slide show for the 390th diorama.
-# Adrian Boyko
+# Adrian Boyko for Xerocraft
 
 import time
 import pygame
-from subprocess import call, DEVNULL
 from neopixel import *
 
 # LED strip configuration:
-LED_COUNT      = 18      # Number of LED pixels.
+LED_COUNT      = 54      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 400000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
@@ -23,7 +22,8 @@ LED_PATTERNS = {
     # Since there is no pattern for slide 2, the pattern for slide 1 will persist.
     3: [12, 14], # LEDs 12 and 14 should be lit for slide 3. 
     4: [], # All the LEDs should be off for slide 4.
-    # Slides 5, 6, 7, 8, and 9 will also have all LEDs off.
+    5: [22, 23, 24, 25, 26, 27],
+    6: [54, 53, 52, 51, 50],
     10: [1,7,8,18], # LEDs 1, 7, 8, and 18 should be lit for slide 10.
 }
 
@@ -37,11 +37,25 @@ def show_slide_leds(strip, slidenum):
  
 def show_slide_image(screen, slidenum):
     filename = "slides/Slide{:02}.JPG".format(slidenum)
-    slide = pygame.image.load(filename)
+    slide = pygame.image.load(filename)    
     slide_rect = slide.get_rect()
-    delta_x = (screen.get_width() - slide_rect.width) / 2.0
-    delta_y = (screen.get_height() - slide_rect.height) / 2.0
+
+    # Resize the images to the screen.
+    # For some reason, all provided images are 960x576, so we're always scaling up. 
+    scale_x = screen.get_width() / slide_rect.width
+    scale_y = screen.get_height() / slide_rect.height
+    scale = min(scale_x, scale_y)
+    scaled_width = int(scale * slide_rect.width)
+    scaled_height = int(scale * slide_rect.height)
+    scaled_size = (scaled_width, scaled_height)
+    slide = pygame.transform.smoothscale(slide, scaled_size)
+
+    # Since the scaled image probably isn't a perfect fit, center it.
+    delta_x = (screen.get_width() - scaled_width) / 2.0
+    delta_y = (screen.get_height() - scaled_height) / 2.0
     slide_rect.move_ip(delta_x, delta_y)
+
+    # Blit the modified slide
     black = 0, 0, 0
     screen.fill(black)
     screen.blit(slide, slide_rect)
@@ -63,11 +77,10 @@ if __name__ == '__main__':
     pygame.init()
     pygame.mouse.set_visible(0)
     size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-    print(size)
     screen = pygame.display.set_mode(size)
 
     while True:
         for s in range(SLIDE_COUNT):
             show_slide(strip, screen, s+1)
-            time.sleep(2)
+            time.sleep(5)
 
